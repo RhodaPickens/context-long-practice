69a0d17109b37c8c5304dc8c3db38f8eb37358b8
Rhoda Pickens <rhodajane@gmail.com>
2023-05-04 14:02:00 -0500

    Completed problems 04 and 05

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,21 @@
+// Calls function with the object as the context
+
 function changeContext(func, obj) {
-  // Your code here
+  return func.call(obj);
 }
+
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// function extractName() {
+//   return this.name;
+// }
+
+// const kristen = new Person('Kristen');
+// console.log(changeContext(extractName, kristen)); // => Kristen
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
