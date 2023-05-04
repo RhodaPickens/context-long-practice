a6384523f3c4f5897d3d1f4fdd88980a6835058e
Rhoda Pickens <rhodajane@gmail.com>
2023-05-04 10:02:08 -0500

    Finished make dog problem

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,4 +1,28 @@
-// Your code here
+class Dog {
+	constructor(name) {
+		this.name = name;
+	}
+
+	static makeJet() {
+		return new Dog ("Jet");
+	}
+
+	changeName(newName) {
+		return this.name = newName;
+	}
+
+	speak(word) {
+		return `${this.name} says ${word}`;
+	}
+}
+
+// let dog1 = Dog.makeJet();
+
+// console.log(dog1.name);
+// console.log(dog1.speak("hello"));
+// console.log(dog1.changeName("Rufus"));
+// console.log(dog1.name);
+// console.log(dog1.speak("woof"));
 
 
 /*****************************************************************************/
