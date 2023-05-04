c46410822ad3073f20ca9666555e5ce90026e4bc
Rhoda Pickens <rhodajane@gmail.com>
2023-05-04 09:43:07 -0500

    Completed calculator problem

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,34 @@
-// Your code here
+class Calculator {
+	constructor() {
+		this.total = 0;
+	}
+
+	add(num) {
+		return this.total += num;
+	}
+
+	subtract(num) {
+		return this.total -= num;
+	}
+
+	divide(num) {
+		this.total /= num;
+		return this.total;
+	}
+
+	multiply(num) {
+		this.total *= num;
+		return this.total;
+	}
+}
+
+// let calculator = new Calculator();
+// console.log(calculator.add(50));      // => 50
+// console.log(calculator.subtract(35)); // => 15
+// console.log(calculator.multiply(10)); // => 150
+// console.log(calculator.divide(5));    // => 30
+// console.log(calculator.total)         // => 30
+
 
 
 /*****************************************************************************/
